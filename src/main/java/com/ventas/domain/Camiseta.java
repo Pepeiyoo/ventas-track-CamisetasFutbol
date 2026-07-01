882141e3704617437c4ae5c3e35caaf6ed57a8de
Pepeiyoo <pcarrascoharo@gmail.com>
2026-07-01 21:16:25 +0200

    primer commit

---
+++ src/main/java/com/ventas/domain/Camiseta.java
@@ -0,0 +1,5 @@
+package com.ventas.domain;
+
+public class Camiseta {
+
+}
---
+++ src/test/java/com/ventas/VentasTrackCamisetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasTrackCamisetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
