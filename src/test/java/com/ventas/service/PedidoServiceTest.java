42afaa339c2d6f3e6212d40173a90c4a85dc0009
Pepeiyoo <pcarrascoharo@gmail.com>
2026-07-02 18:56:46 +0200

    feat: implement order service and mock repository tests with Mockito

---
+++ src/test/java/com/ventas/service/PedidoServiceTest.java
@@ -0,0 +1,40 @@
+package com.ventas.service;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.ventas.domain.EstadoPedido;
+import com.ventas.domain.Pedido;
+import com.ventas.domain.PedidoRepository;
+
+public class PedidoServiceTest {
+
+    @Test
+    public void testProcesarYAsignarPedidoConExito() {
+        // 1. Creamos el Mock (simulacro) del repositorio
+        PedidoRepository repoMock = mock(PedidoRepository.class);
+        
+        // 2. Preparamos un pedido ficticio
+        Pedido pedidoSimulado = new Pedido("PED-200", EstadoPedido.ABIERTO);
+        
+        // 3. Programamos el comportamiento del Mock (When / Then)
+        // "Cuando el servicio busque el ID 'PED-200', devuelve nuestro pedido ficticio"
+        when(repoMock.findById("PED-200")).thenReturn(Optional.of(pedidoSimulado));
+        
+        // 4. Instanciamos el servicio pasándole el repositorio simulado
+        PedidoService pedidoService = new PedidoService(repoMock);
+        
+        // 5. Ejecutamos la acción del servicio
+        pedidoService.procesarYAsignar(String.valueOf("PED-200"), "AdministradorPepe");
+        
+        // 6. Verificaciones
+        assertEquals("AdministradorPepe", pedidoSimulado.getNombreAdministrador());
+        // Verificamos que el servicio realmente llamó al método .save() para guardar los cambios
+        verify(repoMock, times(1)).save(pedidoSimulado);
+    }
+}
